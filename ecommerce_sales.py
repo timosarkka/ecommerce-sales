f76ec367c692917ea0fc9b750c1f1406ab3dd50e
timosarkka <timo.sarkka@skillio.ai>
2024-05-31 18:47:16 +0300

    Added data input, cleaning, manipulation and transformation functions

--- ecommerce_sales.py
+++ ecommerce_sales.py
@@ -4,7 +4,8 @@
 import pandas as pd
 import datetime as dt
 
-pd.options.mode.chained_assignment = None
+pd.options.mode.chained_assignment = None 
+pd.options.display.float_format = '{:,.2f}'.format
 
 # Display all columns
 pd.set_option('display.max_columns', None)
@@ -33,16 +34,30 @@ def manipulate(cleaned_data):
     cleaned_data["order_month"] = cleaned_data["order_date"].dt.month_name()
     return cleaned_data
 
-# Group the data by product_category and summarize total sales
 def transform(manipulated_data):
-    final_data = manipulated_data.groupby("product_category").agg({'total_price': 'sum', 'quantity': 'mean'})
-    return final_data
+    # Group the data by product_category and summarize total sales
+    total_sales_prod_cat = manipulated_data.groupby("product_category").agg({'total_price': 'sum', 'quantity': 'mean'})
+
+    # Pivot the data to create a summary table showing total sales per month for each product category.
+    total_sales_prod_cat_by_month = manipulated_data.groupby(["product_category", "order_month"]).agg({'total_price': 'sum'})
+
+    # Normalize the unit_price column to show prices as a percentage of the average price across all products.
+    avg_price_all_products = manipulated_data['unit_price'].mean()
+    print(avg_price_all_products)
+    manipulated_data['unit_price'] = (manipulated_data['unit_price'] / avg_price_all_products)*100
+
+    return total_sales_prod_cat, total_sales_prod_cat_by_month, manipulated_data
 
 def main():
     raw_data = read_dataset()
     cleaned_data = clean_data(raw_data)
     manipulated_data = manipulate(cleaned_data)
     final_data = transform(manipulated_data)
-    print(final_data)
+    print()
+    print(final_data[0])
+    print()
+    print(final_data[1])
+    print()
+    print(final_data[2])
 
 main()
